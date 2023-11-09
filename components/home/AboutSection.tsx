b7881039471470d551eb42fe17c800613d2f609c
abhay-ahire-ttpl <abhay.ahire.fidel@gmail.com>
2023-11-09 13:50:32 +0530

    Created basic lorem app with next and tailwind

---
+++ components/home/AboutSection.tsx
@@ -0,0 +1,14 @@
+// AboutSection.js
+import React from "react";
+import About from "@/components/about/About";
+
+export default function AboutSection() {
+  return (
+    <div
+      className="lg:mt-[20rem] py-10 mb-[10rem] lg:block md:hidden sm:hidden max-sm:hidden relative backgroundContainer cursor-pointer"
+      id="about"
+    >
+      <About />
+    </div>
+  );
+}
